77fa8dc3f11675c17f9a9767068ed667cd289117
Anand Vardhan <anand.vn100@gmail.com>
2018-08-12 02:56:36 -0500

    Hello World Application Commit

---
+++ src/test/java/com/helloworld/HelloWorldTest.java
@@ -0,0 +1,14 @@
+package com.helloworld;
+
+import org.testng.annotations.Test;
+
+public class HelloWorldTest {
+	
+	@Test
+	public void printHellowWorld()
+	{
+		String helloWorld = "Hello World!!!";
+		System.out.println("Hello World Application name is: "+helloWorld);
+	}
+
+}
